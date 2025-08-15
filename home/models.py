8124af32d446d2e4ddd1d7b36fd5703bb3d58180
Sachin Rajput <sachinrajput30881@gmail.com>
2025-08-15 13:07:16 +0530

    29736

--- home/models.py
+++ home/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length = 100)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="restaurant_images/", blank=True, null=True)
 
     def __str__(self):
         return self.name
