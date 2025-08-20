67f94a60848e2f141fc4ab0fce2148a48fd2de89
Sachin Rajput <sachinrajput30881@gmail.com>
2025-08-20 13:32:46 +0530

    35088

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("about/", views.about, name="about"),
+    path("reservations/", views.reservations, name="reservations"),
 
     
 ]
