945ff44e8763daadd1ac19c0f5abdb931782e19c
Sachin Rajput <sachinrajput30881@gmail.com>
2025-08-20 11:53:01 +0530

    35086

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -117,3 +117,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+RESTAURANT_PHONE = "+919876543210"
