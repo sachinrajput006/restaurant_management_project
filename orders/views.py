0c5db63a8f637e38124c818bcb7802230939f0bb
Sachin Rajput <sachinrajput30881@gmail.com>
2025-09-03 15:20:42 +0530

    40715

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import response
+
+@api_view(["GET"])
+def get_menu(request):
+    """
+    API endpoint to retrieve the restaurant's menu.
+    Hardcoded menu data for now.
+
+    """
+
+    menu = [
+        {
+            "name": "Margherita Pizza",
+            "description": "Clasic pizza with fresh tomatoes, mozzarella cheese, and basil.",
+            "price": 250
+
+        },
+        {
+            "name": "Veggie Burger",
+            "description": "Grilled veggie patty with lecture, tomato, and vegan mayo.",
+            "price": 850
+        },
+        {
+            "name": "Pasta Alfredo",
+            "description": "Creamy white sauce pasta with mushrooms and herbs.",
+            "price": 850
+        }
+
+    
+
+    ]
+
+    return Response(menu)
