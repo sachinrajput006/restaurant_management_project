5704c9f62d6fef1bdd685cce0796200de5da554b
Sachin Rajput <sachinrajput30881@gmail.com>
2025-09-05 11:48:52 +0530

    42505

--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from.forms import ContactForm
 
-# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = ContactForm()
+
+    return render(request, "home.html",{"form": form})
+
+
+
