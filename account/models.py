061ea54cd82e8295f76e80405ad2a654d906f781
Sachin Rajput <sachinrajput30881@gmail.com>
2025-09-05 12:13:40 +0530

    42505

--- account/models.py
+++ account/models.py
@@ -17,3 +17,11 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_add_now=True)
+
+    def __str__(self):
+        return self.name
