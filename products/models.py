f425fd702b29da33b3310ced4fdcfe710192261c
Sachin Rajput <sachinrajput30881@gmail.com>
2025-09-07 17:18:19 +0530

    42507

--- products/models.py
+++ products/models.py
@@ -8,3 +8,13 @@ class Item(models.Model):
 
     def __str__(self):
         return str(self.item_name)
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+    def __str__(self):
+        return f"{self.name} -{self.price}"
