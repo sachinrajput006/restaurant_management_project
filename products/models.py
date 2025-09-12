424441c6360f6c73656ec8da71fbac1e7f9b937e
Sachin Rajput <sachinrajput30881@gmail.com>
2025-09-12 12:08:47 +0530

    43429

--- products/models.py
+++ products/models.py
@@ -27,3 +27,13 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class ContactSumbission(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    sumbitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
