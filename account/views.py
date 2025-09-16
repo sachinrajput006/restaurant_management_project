5d52c90be551b175fbe7a99f28faa08550c0ceaf
Sachin Rajput <sachinrajput30881@gmail.com>
2025-09-16 11:28:27 +0530

    44339

--- account/views.py
+++ account/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, redirect
 from.forms import ContactForm
+form .models import MenuItem
 
 def home(request):
+    query = request.GET.get("q", "")
+    if query:
+        menu_items = MenuItem.objects.filter(name__icontains=query)
+    else:
+        menu_items = MenuItem.objects.all()
+
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -10,7 +17,11 @@ def home(request):
     else:
         form = ContactForm()
 
-    return render(request, "home.html",{"form": form})
+    return render(request, "home.html",{
+        "form": form,
+        "menu_items": menu_items,
+        "query": query
+        })
 
 
 
