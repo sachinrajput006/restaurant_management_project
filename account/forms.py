50592d148628e1e5c3fb4494887807a99685286a
Sachin Rajput <sachinrajput30881@gmail.com>
2025-09-16 11:42:01 +0530

    44340

--- account/forms.py
+++ account/forms.py
@@ -1,9 +1,7 @@
-from djongo import forms
-from .models import Contact
+from django import forms
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ['name', 'email']
-
-        
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length = 100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
+    
