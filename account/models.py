beb34f3cde622880c1d2ed076463422b5de6067a
Sachin Rajput <sachinrajput30881@gmail.com>
2025-09-16 12:29:40 +0530

    44341

--- account/models.py
+++ account/models.py
@@ -25,3 +25,11 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+class Menuitem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to="menu_images/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
